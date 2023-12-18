5ba6fe77866044b424f20b70cffe621bfbf0feb3
Jose Marozzi <marozzi20@gmail.com>
2023-12-18 18:32:10 -0300

    agregado bucle

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
 
 function calcularPrestamo() {
-    const tasaInteres = 0.35; // 35% de interés anual
-    
-    // Pregunta al usuario cuánto dinero necesita
-    const monto = parseFloat(prompt("¿Cuánto dinero necesita?"));
-    
-    if (isNaN(monto)) {
-      alert("Por favor, ingrese un monto válido.");
-    } else {
-      const plazo = parseInt(prompt("¿En cuántos meses planea pagar el préstamo?"));
-      
-      if (isNaN(plazo)) {
-        alert("Por favor, ingrese un plazo válido.");
-      } else {
-        const tasaMensual = tasaInteres / 12;
-        const cuotaMensual = (monto * tasaMensual) / (1 - Math.pow(1 + tasaMensual, -plazo));
-        const totalAPagar = cuotaMensual * plazo;
-        
-        alert(`
-          Monto del préstamo: $${monto.toFixed(2)}
-          Plazo: ${plazo} meses
-          Cuota mensual: $${cuotaMensual.toFixed(2)}
-          Total a pagar: $${totalAPagar.toFixed(2)}`);
-      }
-    }
-  }
+  const tasaInteres = 0.35; // 35% de interés anual
   
-  // Llama a la función al cargar la página para iniciar el proceso
-  calcularPrestamo();
+  let monto, plazo;
+
+  // Bucle para obtener un monto válido
+  while (isNaN(monto) || monto <= 0) {
+      monto = parseFloat(prompt("¿Cuánto dinero necesita?"));
+      if (isNaN(monto) || monto <= 0) {
+          alert("Por favor, ingrese un monto válido.");
+      }
+  }
+
+  // Bucle para obtener un plazo válido
+  while (isNaN(plazo) || plazo <= 0) {
+      plazo = parseInt(prompt("¿En cuántos meses planea pagar el préstamo?"));
+      if (isNaN(plazo) || plazo <= 0) {
+          alert("Por favor, ingrese un plazo válido.");
+      }
+  }
+
+  const tasaMensual = tasaInteres / 12;
+  const cuotaMensual = (monto * tasaMensual) / (1 - Math.pow(1 + tasaMensual, -plazo));
+  const totalAPagar = cuotaMensual * plazo;
+
+  alert(`
+      Monto del préstamo: $${monto.toFixed(2)}
+      Plazo: ${plazo} meses
+      Cuota mensual: $${cuotaMensual.toFixed(2)}
+      Total a pagar: $${totalAPagar.toFixed(2)}`);
+}
+
+// Llama a la función al cargar la página para iniciar el proceso
+calcularPrestamo();
